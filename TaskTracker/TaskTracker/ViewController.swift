165d42d3bc4291b24791ec4564ece1d762ef5dca
eisenbergm2019 <97626477+eisenbergm2019@users.noreply.github.com>
2023-11-05 09:51:23 -0500

    Add files via upload

---
+++ TaskTracker/TaskTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskTracker
+//
+//  Created by Matthew Eisenberg on 11/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TaskTracker/TaskTrackerUITests/TaskTrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TaskTrackerUITestsLaunchTests.swift
+//  TaskTrackerUITests
+//
+//  Created by Matthew Eisenberg on 11/5/23.
+//
+
+import XCTest
+
+final class TaskTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
