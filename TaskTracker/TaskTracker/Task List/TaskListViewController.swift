77fe7248408d187dd423ec7e66e4c32a53aabeea
eisenbergm2019 <97626477+eisenbergm2019@users.noreply.github.com>
2023-11-20 10:39:53 -0500

    Add files via upload
    fixed issues

--- TaskTracker/TaskTracker/Task List/TaskListViewController.swift
+++ TaskTracker/TaskTracker/Task List/TaskListViewController.swift
@@ -19,6 +19,21 @@ class TaskListViewController: UIViewController {
         }
     }
 
+    @IBAction func OnLogOutTapped(_ sender: Any) {
+        showConfirmLogoutAlert()
+    }
+    
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Log out of your account?", message: nil, preferredStyle: .alert)
+        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(logOutAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
